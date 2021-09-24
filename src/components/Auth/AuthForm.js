5a43256f6a9518cc27b34c356ee7f8dbb8451b8f
Gary Leija <gleija@seek.com.au>
2021-09-24 16:24:54 +1000

    Add user sign up

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,67 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
-  const switchAuthModeHandler = () => {
+  const switchAuthModeHandler = (event) => {
+    event.preventDefault();
     setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    // optional Add Validation
+    if (isLogin) {
+    } else {
+      // Please add your own firebase API public key below
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbd96S32LNTWO_mLo67vB_2fWomfxe0sI",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          console.log(res);
+        } else {
+          res.json().then((data) => {
+            // show some sort of error
+            console.log(data);
+          });
+        }
+      });
+    }
   };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" ref={emailInputRef} id="email" required />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            ref={passwordInputRef}
+            id="password"
+            required
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
